fbe951481dc698ea0afc28fc5618cddddfac17e6
Jyoti Suman <135809676+jyotisuman-web@users.noreply.github.com>
2024-08-07 14:44:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
 
-let inputSearch=document.querySelector(".searchinplutcont")
-let recentInput=[]
-let formInput1=document.querySelector("inputform")
-const listOfRecent1=document.querySelector(".listofRecent")
 
-inputSearch.addEventListener("keydown",() => {
-    if (inputSearch.value){
-        document.getElementById("a fa-search").style.display="block"
-    }
-    else{
-        document.getElementById("fa fa-search").style.display="none"
-    }
-})
-formInput1.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    let listOfRecenthtml1=""
-    recentInput.unshift(inputSearch.value)
-    console.log(recentInput)
-    if (recentInput.length>0){
-        for (let i=0; i<recentInput.length;i++){
-            listOfRecent1+=`
-            <div class="listofRecent">
-            <p>${recentInput[i]}</p>
-            <div>
-            `
-        }
-        listOfRecent1.innerHTML=listOfRecenthtml1
-    }
-})
+//for mobile slideDown
+function openNav(){
+    document.getElementById("slidedown").style.width="100%";
+}
+
+function closeNav(){
+    document.getElementById("slidedown").style.width="0%";
+}
 
 //for mobile slideDown
 function openNav(){
@@ -36,4 +16,50 @@ function openNav(){
 function closeNav(){
     document.getElementById("slidedown").style.width="0%";
 }
+//filter images
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("columns");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
 
